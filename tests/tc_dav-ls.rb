bb286865206aab34cfb0b2f64be280b95a0aad09
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-09-04 12:20:04 +0200

    relative url support

--- tests/tc_dav-ls.rb
+++ tests/tc_dav-ls.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+require 'rubygems'
 require 'test/unit'
 require 'davclient'
 require 'davclient/dav-ls'
@@ -10,10 +11,11 @@ class TestWDavLs < Test::Unit::TestCase
   # Run the 'wdav ls' command line script and capture stdin & stderr
   def ls(*args)
     out, err = util_capture do
-      WDav.ls(args)
+      LsCLI.ls(args)
     end
     return [out.string, err.string]
   end
+
 
   def test_basic_ls
     out, err = ls("-a", "https://vortex-dav.uio.no/prosjekter/it-avisa/nyheter/2006/")
@@ -28,6 +30,8 @@ class TestWDavLs < Test::Unit::TestCase
   def test_ls_cwurl
     out, err = ls("-a", "https://vortex-dav.uio.no/prosjekter/it-avisa/nyheter/2006/")
     assert_equal(7, out.split(/\n/).size )
+
+    WebDAV.cd("https://vortex-dav.uio.no/prosjekter/it-avisa/nyheter/2006/")
     out, err = ls("-a") # If no url are given, should use the last used url
     assert_equal(7, out.split(/\n/).size )
 
