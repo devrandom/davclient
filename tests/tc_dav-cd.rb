a643dca65c4044b6244185a609ee9fc71ea3214f
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-09-04 12:19:59 +0200

    relative url support

--- tests/tc_dav-cd.rb
+++ tests/tc_dav-cd.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+require 'rubygems'
 require 'test/unit'
 require 'test_helper'
 require 'davclient'
@@ -8,17 +9,37 @@ require 'test/zentest_assertions'
 class TestWDavCd < Test::Unit::TestCase
 
   # Run the wdav_ls command line script and capture stdio
-  def cd(*args)
+  def cd(args)
     out, err = util_capture do
-      WDav.cd(args)
+      WebDAV.cd(args)
     end
     return [out.string, err.string]
   end
 
   def test_basic_cd
-    out, err = cd("https://vortex-dav.uio.no/prosjekter/it-avisa/nyheter/2006/")
-    # puts "DEBUG: " + out
-    assert out =~ /Set WebDAV/
+    url = "https://vortex-dav.uio.no/prosjekter/it-avisa/nyheter/2006/"
+    out, err = cd(url)
+    assert_equal url, WebDAV.CWURL
+  end
+
+  def test_pathname
+    url = "https://vortex-dav.uio.no/prosjekter/it-avisa/nyheter/2006/"
+    out, err = cd(url)
+    assert_equal url, WebDAV.CWURL
+    out, err = cd("..")
+    assert_equal url = "https://vortex-dav.uio.no/prosjekter/it-avisa/nyheter/", WebDAV.CWURL
+
+    out, err = WebDAV.cd("../../../brukere/thomasfl/")
+    assert_equal "https://vortex-dav.uio.no/brukere/thomasfl/", WebDAV.CWURL
+
+    exception = false
+    begin
+      out, err = WebDAV.cd("../../../../../../")
+    rescue Exception
+      exception = true
+    end
+    assert exception, "Should have raised an exception"
+
   end
 
 end
