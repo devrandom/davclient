b7e8ffe3ddb74c567ad8fba3229261bdc8a6d145
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-09-18 15:29:25 +0200

    added put command

--- lib/davclient/dav-put.rb
+++ lib/davclient/dav-put.rb
@@ -7,72 +7,79 @@ require 'optparse'
 class PutCLI
 
   def self.put(args)
+
     options = read_options(args)
     url = args[0]
     if(options[:string])then
+
+      # Put string
+      if(!url.match(/^http.*\/\/([^\/]*)/) and !WebDAV.CWURL)
+        raise "Error: No current working url set. Use '#{$0} cd url' to set url."
+      end
+
       begin
         WebDAV.put_string(url,options[:string])
       rescue
         puts $0 + ": " + $!
       end
-      puts "Published content to " + url
+      puts "Published content to: " + url
     else
 
-      #
-      # Put file
-      #
+      # Put files(s)
 
-      puts "DEBUG: size:" + args.size.to_s
+      #  puts "DEBUG: size:" + args.size.to_s
 
       if(args.size == 1 )
         local_file = args[0]
         if(not(File.exists?(local_file)))
-          puts "File: #{local_file} doesn't exist."
-          # raise ...
-          exit
+          raise "File not found: #{local_file}"
         end
         if(!WebDAV.CWURL)
-          puts "Error: No current working url set. Use '#{$0} cd url' to set url."
-          # raise ..
-          exit
+          raise "Error: No current working url set. Use '#{$0} cd url' to set url."
         end
 
         WebDAV.put(WebDAV.CWURL, local_file)
 
       elsif(args.size == 2 and args[1].match(/^http.*\/\/([^\/]*)/) )
         local_file = args[0]
-
-        # TODO: Use Pathname ?
-        if(not(local_file =~ /^\//))
-          puts "DEBUG: use pathname: " + local_file
-          puts "not implemented"
-          exit
-        end
+        url = args[1]
 
         if(not(File.exists?(local_file)))
-          puts "File: #{local_file} doesn't exist."
-          # raise ...
-          exit
+          raise "File not found: #{local_file}"
         end
 
-        url = args[1]
         if(WebDAV.isCollection?(url))
-          # puts "DEBUG; isCollection"
           url += File.basename(local_file)
         end
 
-        # puts "ok: " + url + " " + local_file
         WebDAV.put(url, local_file)
+
       else
 
-        args.each do | arg|
-          puts "arg:" + arg
-        end
-        # TODO
-        #  - 2 args: 1. local-file 2.url-med-filnavn
-        #  - Siste arg =~ http?://
-        puts "PUT file not implemented"
+        # Put more than one file
 
+        if(args.last.match(/^http.*\/\/([^\/]*)/) )
+           url = args.last
+           if(!WebDAV.isCollection?(url))
+             raise "Destination collection not found: " + url
+           end
+           args = args[0..(args.size-2)]
+        else
+          url = WebDAV.CWURL
+        end
+
+        count = 0
+        args.each do | arg|
+          # puts "arg:" + arg
+          if(File.exists?(arg))
+            basename = File.basename(arg)
+            WebDAV.put(url + basename, arg)
+            count = count + 1
+          else
+            raise "Error: File not found " + arg
+          end
+        end
+        puts "Published #{count} files to #{url}"
       end
     end
 
@@ -80,23 +87,29 @@ class PutCLI
 
   private
 
-  def self.usage()
-    puts "Usage: #{$0} put --string \"<html>..</html>\" url"
-  end
-
   def self.read_options(args)
     options = {}
 
     optparse = OptionParser.new do|opts|
-      opts.banner = "Usage: #{$0} put [options] url"
+      opts.banner = "Usage: #{$0} put [options] [filelist][url]"
 
       opts.on( '-h', '--help', 'Display this screen' ) do
         puts opts
+        puts
+        puts "Upload local file or files to server."
+        puts
+        puts "Examples:"
+        puts
+        puts "       #{$0} put local_file"
+        puts "       #{$0} put local_filename https://dav.org/remote_filename"
+        puts "       #{$0} put *.html https://dav.org/remote_collection/"
+        puts "       #{$0} put --string \"Hello world\" hello_world.html"
+        puts
         exit
       end
 
       options[:string] = false
-      opts.on( '-s', '--string', "Put contents of string" ) do |str |
+      opts.on( '-s', '--string STRING', "Put contents of string" ) do |str |
         options[:string] = str
       end
 
