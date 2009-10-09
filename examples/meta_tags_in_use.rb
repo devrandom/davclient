8209a4b84ed8adc15ac1e6f8d4cc71d0112095b0
Thomas Flemming <thomas.flemming@usit.uio.no>
2009-10-09 12:25:53 +0200

    added examples

---
+++ examples/meta_tags_in_use.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'davclient/simple'
+
+# Prints out content of <meta > tags found in files
+
+def print_html_meta_elements(url)
+  doc = Hpricot(get(url))
+  if(doc.search("//meta"))
+
+    puts url
+    doc.search("//meta").each do |elem|
+      if(elem.attributes.key?("name") )
+        print " Name: " + elem.attributes["name"].ljust(30)
+      end
+      if(elem.attributes.key?("content") )
+        print " Content: " + elem.attributes["content"]
+      end
+      puts
+    end
+    puts
+  end
+end
+
+
+cd "https://www-dav.jus.uio.no/it/"
+
+find :recursive => true do |item|
+  url =  item.href.to_s
+  if(url =~ /htm?|\.xml$/ )
+    print_html_meta_elements(url)
+  end
+
+end
